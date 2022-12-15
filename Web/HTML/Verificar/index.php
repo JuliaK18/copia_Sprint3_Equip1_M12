c1fcb32e69468072a8d8a5f147865e5db4e86a2b
Kizumi19 <juliakrukonis@iesmontsia.org>
2022-12-15 20:59:45 +0100

    Actualització verificar usuari

--- Web/HTML/Verificar/index.php
+++ Web/HTML/Verificar/index.php
@@ -24,6 +24,7 @@ include_once "../../PHP/Classes/Class_Usuaris.php";
         Verificar
       </button>
 
+   
       <!-- Inici Modal -->
       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -33,23 +34,32 @@ include_once "../../PHP/Classes/Class_Usuaris.php";
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">  <!-- Finestra que mostra usuaris que sol·liciten la verificació -->
-            <?php
-                    $verify = Usuari::check_not_verify_user();
-                    ?>
 
-                  
-                  <div class='input-group'>
-                    <input type='text' class='form-control' placeholder='Nom usuari'>
-                    <!-- -->
-                    <form action="POST">
-                      
-                    </form>
-                  </div> 
-                 
-              
+              <!-- Mostrar usuaris no verificats-->
+              <?php
+            $consulta = Usuari::get_users_not_verified2();
+            while ($mostrar = mysqli_fetch_array($consulta)) {
+            ?>
+              <tr>
+                <td id="user"><?php echo $mostrar['user'] ?></td>
+              </tr>
+
+              <br>
+              <br>                      
+                    <form method="POST" id="verify" action = "verify.php">
+                      <div class="input-group">
+                      <?php
+                       }
+                      ?>
+                          <label for="message-text" class="col-form-label">Nom usuari: </label>
+                          <input class="form-control" name="user" id="user">
+                      </div>
+                        </div>
+                    </form>    
+                   
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-primary">Guardar canvis</button>
+              <button onclick="confirmForm()" type="button" class="btn btn-primary" id ="si">Guardar canvis</button>
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tanca</button>
             </div>
           </div>
@@ -57,52 +67,13 @@ include_once "../../PHP/Classes/Class_Usuaris.php";
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <!-- Final Modal -->
 
-&nbsp;
+<br>
 
-    <!-- Començament del Button trigger modal -->
-    <div class="container">
-      <h2>Llistat usuaris per treure la verificació</h2>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Desverificar
-      </button>
-
-      <!-- Inici Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel"> Insereix el nom de l'usuari per desverificar</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">  <!-- Finestra que mostra usuaris que sol·liciten la verificació -->
-            <?php
-                    $verify = Usuari::check_not_verify_user();
-                    ?>
-
-                  
-                  <div class='input-group'>
-                    
-                    <input type='text' class='form-control' placeholder='Nom usuari'>
-                    
-                  </div> 
-                 
-              
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-primary">Guardar canvis</button>
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tanca</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-
-<!-- Final Modal -->
+    
 
 
     
--- Web/HTML/Verificar/verificacio.js
+++ Web/HTML/Verificar/verificacio.js
@@ -1,7 +1,22 @@
+//Confirmació per poder assegurar-nos si volem verificar a l'usuari mencionat 
+document.getElementById("si").onclick = confirmForm;
 
+function confirmForm(){
+    if(confirm('Estàs segur de validar aquest usuari?')){
+        return boolean;
+    }     
+}
 
-$.ajax({
-    url: 'index.php',  // L'arxiu PHP on es troba el mètode
-    type: 'POST',  // El tipus de solicitut que s'està enviant (POST, GET, etc.)
-    data: { method: 'check_verify_user' }  // Les dades que s'están enviant l'arxiu PHP (amb el nom del mètode)
-  })
+// CapacityWindow.document.open("text/html");
+// CapacityWindow.document.write(s);
+// CapacityWindow.document.close();
+
+// //Començament ajax
+// $.ajax({
+//     url: 'index.php',  // L'arxiu PHP on es troba el mètode
+//     type: 'POST',  // El tipus de solicitut que s'està enviant (POST, GET, etc.)
+//     data: { method: 'check_verify_user' }  // Les dades que s'están enviant l'arxiu PHP (amb el nom del mètode)
+//     success:function(resultat){
+//         console.log(result.prova);
+//     }
+//   });
